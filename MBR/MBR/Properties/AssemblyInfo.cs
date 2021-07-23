d9997ec914868d0e476b3c0cd290e9b7199ebd78
Vichingo455 <59311016+Vichingo455@users.noreply.github.com>
2021-07-23 10:17:38 +0200

    Update AssemblyInfo.cs

--- MBR/MBR/Properties/AssemblyInfo.cs
+++ MBR/MBR/Properties/AssemblyInfo.cs
@@ -6,10 +6,10 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("MBR")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Clutt4 MBR helper")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MBR")]
+[assembly: AssemblyProduct("Clutt4")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
